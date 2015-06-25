d59d39d594c3cacfeafebbc658f6dcc0db57de59
Alexander Mezhov <as.mezhov@gmail.com>
2015-06-25 09:09:44 +0500

    Docs: Element

--- InfinniPlatform.Api/Properties/AssemblyInfo.cs
+++ InfinniPlatform.Api/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 4
+// 5
 // e0ecaf6a-c2c2-4c8a-be1e-af6444fe8342
-// D893E672E908D56674ABAD5967B923A9
+// D1FADC038CAB6AE8F09EA68394EB40F4
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e0ecaf6a-c2c2-4c8a-be1e-af6444fe8342")]
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
 [assembly: InternalsVisibleTo("InfinniPlatform.Api.Tests")]
 [assembly: AssemblyDescription("InfinniPlatform SDK for .NET")]
