88be9c4a64a0e1436f55482c1ec2084d47a21c22
Alexander Mezhov <as.mezhov@gmail.com>
2015-07-04 17:59:18 +0500

    Docs: EditMask

--- InfinniPlatform.Api/Properties/AssemblyInfo.cs
+++ InfinniPlatform.Api/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 24
+// 25
 // e0ecaf6a-c2c2-4c8a-be1e-af6444fe8342
-// 7C060188DFDDCD6AAFCDD160D1C0D4D5
+// CF16FF2643E210121C16B824F3191D62
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e0ecaf6a-c2c2-4c8a-be1e-af6444fe8342")]
-[assembly: AssemblyVersion("1.0.0.24")]
-[assembly: AssemblyFileVersion("1.0.0.24")]
+[assembly: AssemblyVersion("1.0.0.25")]
+[assembly: AssemblyFileVersion("1.0.0.25")]
 [assembly: InternalsVisibleTo("InfinniPlatform.Api.Tests")]
 [assembly: AssemblyDescription("InfinniPlatform SDK for .NET")]
