e641775e00b902aec6b25e96a9ccf0d16364f409
Alexander Mezhov <as.mezhov@gmail.com>
2015-07-06 21:13:46 +0500

    Docs: Merge

--- InfinniPlatform.Api/Properties/AssemblyInfo.cs
+++ InfinniPlatform.Api/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 32
+// 33
 // e0ecaf6a-c2c2-4c8a-be1e-af6444fe8342
-// 6627BC2AC05243B099861C453D5A3721
+// DA27993AE3B402A2179856E6F904BFB2
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e0ecaf6a-c2c2-4c8a-be1e-af6444fe8342")]
-[assembly: AssemblyVersion("1.0.0.32")]
-[assembly: AssemblyFileVersion("1.0.0.32")]
+[assembly: AssemblyVersion("1.0.0.33")]
+[assembly: AssemblyFileVersion("1.0.0.33")]
 [assembly: InternalsVisibleTo("InfinniPlatform.Api.Tests")]
 [assembly: AssemblyDescription("InfinniPlatform SDK for .NET")]
