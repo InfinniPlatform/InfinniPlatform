0cca39263505b3ff9d0938a132d0eb537e0766ad
Andrey Popov <andrey.popov.chel@gmail.com>
2015-07-09 14:19:59 +0500

    Add missing file

---
+++ InfinniPlatform.Core/Index/IndexObjectStatus.cs
@@ -0,0 +1,11 @@
+namespace InfinniPlatform.Index
+{
+    public static class IndexObjectStatus
+    {
+        public static string Deleted = "deleted";
+
+        public static string Invalid = "invalid";
+
+        public static string Valid = "valid";
+    }
+}
