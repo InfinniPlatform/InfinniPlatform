717ce1a79a34aac99fbc5dd1a4eb104f9d998b61
Andrey Popov <andrey.popov.chel@gmail.com>
2015-07-17 14:54:05 +0500

    Add missing files

---
+++ InfinniPlatform.RestfulApi/ActionUnits/ActionUnitGetIndexStorageInfo.cs
@@ -0,0 +1,13 @@
+﻿using InfinniPlatform.Api.ContextTypes;
+using InfinniPlatform.RestfulApi.Extensions;
+
+namespace InfinniPlatform.RestfulApi.ActionUnits
+{
+    public sealed class ActionUnitGetIndexStorageInfo
+	{
+		public void Action(IApplyResultContext target)
+		{
+		    target.Result = IndexedStorageExtension.GetStatus();
+		}
+	}
+}
