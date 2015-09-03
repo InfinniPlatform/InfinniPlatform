c039aa81d72f9adfb77ab5518635ad9c2b837b7c
AlexMAS <as.mezhov@gmail.com>
2015-09-03 14:00:18 +0500

    AppSettings & Linux/Mono

--- InfinniPlatform.Api/Properties/AssemblyInfo.cs
+++ InfinniPlatform.Api/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 47
+// 48
 // e0ecaf6a-c2c2-4c8a-be1e-af6444fe8342
-// 2B683156DCA4B413C42A390C3E375B3D
+// F4C549B494204994CAF06D7A0ACA9F25
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e0ecaf6a-c2c2-4c8a-be1e-af6444fe8342")]
-[assembly: AssemblyVersion("1.0.0.47")]
-[assembly: AssemblyFileVersion("1.0.0.47")]
+[assembly: AssemblyVersion("1.0.0.48")]
+[assembly: AssemblyFileVersion("1.0.0.48")]
 [assembly: InternalsVisibleTo("InfinniPlatform.Api.Tests")]
 [assembly: AssemblyDescription("InfinniPlatform SDK for .NET")]
--- InfinniPlatform.Api/Settings/AppSettings.cs
+++ InfinniPlatform.Api/Settings/AppSettings.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
-using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace InfinniPlatform.Api.Settings
 {
@@ -10,6 +12,15 @@ namespace InfinniPlatform.Api.Settings
 	/// </summary>
 	public static class AppSettings
 	{
+		static AppSettings()
+		{
+			AppDomainSettings = new Lazy<Dictionary<string, string>>(GetAppDomainSettings);
+		}
+
+
+		private static readonly Lazy<Dictionary<string, string>> AppDomainSettings;
+
+
 		/// <summary>
 		/// Gets the setting value associated with the specified name.
 		/// </summary>
@@ -17,7 +28,7 @@ namespace InfinniPlatform.Api.Settings
 		/// <param name="defaultValue">Default value of the setting (if not defined).</param>
 		public static string GetValue(string name, string defaultValue = null)
 		{
-			var value = ConfigurationManager.AppSettings[name];
+			var value = GetSettingValue(name);
 
 			return value ?? defaultValue;
 		}
@@ -33,7 +44,7 @@ namespace InfinniPlatform.Api.Settings
 		{
 			var result = defaultValue;
 
-			var value = ConfigurationManager.AppSettings[name];
+			var value = GetSettingValue(name);
 
 			if (string.IsNullOrEmpty(value) == false)
 			{
@@ -54,7 +65,7 @@ namespace InfinniPlatform.Api.Settings
 		{
 			var result = defaultValue;
 
-			var value = ConfigurationManager.AppSettings[name];
+			var value = GetSettingValue(name);
 
 			if (string.IsNullOrEmpty(value) == false)
 			{
@@ -68,6 +79,57 @@ namespace InfinniPlatform.Api.Settings
 			}
 
 			return result;
+		}
+
+
+		private static Dictionary<string, string> GetAppDomainSettings()
+		{
+			// Это оказался самый рабочий и надежный способ в Mono/Linux!
+
+			var appSettings = new Dictionary<string, string>();
+
+			try
+			{
+				var configFile = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile;
+
+				if (!string.IsNullOrEmpty(configFile) && File.Exists(configFile))
+				{
+					var config = XDocument.Load(configFile);
+
+					if (config.Document != null && config.Document.Root != null)
+					{
+						var appSettingsNodes = config.Document.Root
+							.Elements("appSettings")
+							.SelectMany(i => i.Elements("add"))
+							.Select(i => new { key = i.Attribute("key"), value = i.Attribute("value") });
+
+						foreach (var item in appSettingsNodes)
+						{
+							if (item.key != null && !string.IsNullOrEmpty(item.key.Value))
+							{
+								appSettings[item.key.Value] = (item.value != null) ? item.value.Value : null;
+							}
+						}
+					}
+				}
+			}
+			catch
+			{
+			}
+
+			return appSettings;
+		}
+
+		private static string GetSettingValue(string key)
+		{
+			string value;
+
+			if (!string.IsNullOrEmpty(key) && AppDomainSettings.Value.TryGetValue(key, out value))
+			{
+				return value;
+			}
+
+			return null;
 		}
 	}
 }
