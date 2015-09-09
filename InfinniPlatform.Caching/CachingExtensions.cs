870a87decf61bec22c39a8288a821e6dd8300e5c
AlexMAS <as.mezhov@gmail.com>
2015-09-09 16:55:00 +0500

    Add Cache Implementation (Memory & Redis)

---
+++ InfinniPlatform.Caching/CachingExtensions.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Threading;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace InfinniPlatform.Caching
+{
+	public static class CachingExtensions
+	{
+		internal const int AttempCount = 5;
+
+		internal const int AttempDelay = 10 * 1000;
+
+
+		/// <summary>
+		/// Пытается выполнить метод за указанное количество попыток и с указанным ожиданием между попытками.
+		/// </summary>
+		/// <param name="action">Метод.</param>
+		/// <param name="attempCount">Количество попыток.</param>
+		/// <param name="attempDelay">Ожиданием между попытками.</param>
+		internal static void TryExecute(Action action, int attempCount = AttempCount, int attempDelay = AttempDelay)
+		{
+			TryExecute<object>(() => { action(); return null; }, attempCount, attempDelay);
+		}
+
+		/// <summary>
+		/// Пытается выполнить метод за указанное количество попыток и с указанным ожиданием между попытками.
+		/// </summary>
+		/// <param name="action">Метод.</param>
+		/// <param name="attempCount">Количество попыток.</param>
+		/// <param name="attempDelay">Ожиданием между попытками.</param>
+		internal static T TryExecute<T>(Func<T> action, int attempCount = AttempCount, int attempDelay = AttempDelay)
+		{
+			Exception error = null;
+
+			for (var i = 1; i <= attempCount; ++i)
+			{
+				try
+				{
+					return action();
+				}
+				catch (Exception e)
+				{
+					error = e;
+				}
+
+				if (i < attempCount)
+				{
+					Thread.Sleep(i * attempDelay);
+				}
+			}
+
+			throw error ?? new InvalidOperationException();
+		}
+
+
+		/// <summary>
+		/// Возвращает ключ кэширования с указанием пространства имен.
+		/// </summary>
+		/// <param name="unwrappedKey">Ключ кэширования без указания пространства имен.</param>
+		/// <param name="name">Пространство имен.</param>
+		/// <returns>Ключ кэширования с указанием пространства имен.</returns>
+		internal static string WrapCacheKey(this string unwrappedKey, string name)
+		{
+			return string.Format("{0}.{1}", name, unwrappedKey);
+		}
+
+		/// <summary>
+		/// Возвращает ключ кэширования без указания пространства имен.
+		/// </summary>
+		/// <param name="wrappedKey">Ключ кэширования с указанием пространства имен.</param>
+		/// <param name="name">Пространство имен.</param>
+		/// <returns>Ключ кэширования без указания пространства имен.</returns>
+		internal static string UnwrapCacheKey(this string wrappedKey, string name)
+		{
+			string unwrappedKey = null;
+
+			if (!string.IsNullOrEmpty(name))
+			{
+				if (!string.IsNullOrEmpty(wrappedKey) && wrappedKey.StartsWith(name + "."))
+				{
+					unwrappedKey = wrappedKey.Substring(name.Length + 1);
+				}
+			}
+			else
+			{
+				unwrappedKey = wrappedKey;
+			}
+
+			return unwrappedKey;
+		}
+
+
+		public static object GetObject(this ICache cache, string key)
+		{
+			object value;
+
+			TryGetObject(cache, key, out value);
+
+			return value;
+		}
+
+		public static bool TryGetObject(this ICache cache, string key, out object value)
+		{
+			if (string.IsNullOrEmpty(key))
+			{
+				throw new ArgumentNullException("key");
+			}
+
+			string stringValue;
+
+			if (cache.TryGet(key, out stringValue))
+			{
+				value = JToken.Parse(stringValue);
+				return true;
+			}
+
+			value = null;
+			return false;
+		}
+
+		public static void SetObject(this ICache cache, string key, object value)
+		{
+			if (string.IsNullOrEmpty(key))
+			{
+				throw new ArgumentNullException("key");
+			}
+
+			if (value == null)
+			{
+				throw new ArgumentNullException("value");
+			}
+
+			string stringValue = JsonConvert.SerializeObject(value);
+			cache.Set(key, stringValue);
+		}
+	}
+}
---
+++ InfinniPlatform.Caching/Redis/RedisCacheImpl.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+
+using StackExchange.Redis;
+
+namespace InfinniPlatform.Caching.Redis
+{
+	/// <summary>
+	/// Реализует интерфейс для управления распределенным кэшем на базе Redis.
+	/// </summary>
+	public sealed class RedisCacheImpl : ICache, IDisposable
+	{
+		/// <summary>
+		/// Конструктор.
+		/// </summary>
+		/// <param name="name">Пространство имен для ключей.</param>
+		/// <param name="connectionString">Строка подключения к Redis.</param>
+		/// <exception cref="ArgumentNullException"></exception>
+		public RedisCacheImpl(string name, string connectionString)
+		{
+			if (string.IsNullOrEmpty(name))
+			{
+				throw new ArgumentNullException("name");
+			}
+
+			if (string.IsNullOrEmpty(connectionString))
+			{
+				throw new ArgumentNullException("connectionString");
+			}
+
+			_name = name;
+			_database = new Lazy<IDatabase>(() => CreateDatabase(connectionString));
+		}
+
+
+		private readonly string _name;
+		private readonly Lazy<IDatabase> _database;
+
+
+		public bool Contains(string key)
+		{
+			if (string.IsNullOrEmpty(key))
+			{
+				throw new ArgumentNullException("key");
+			}
+
+			return TryExecute((d, k) => d.KeyExists(k), key);
+		}
+
+		public string Get(string key)
+		{
+			string value;
+
+			TryGet(key, out value);
+
+			return value;
+		}
+
+		public bool TryGet(string key, out string value)
+		{
+			if (string.IsNullOrEmpty(key))
+			{
+				throw new ArgumentNullException("key");
+			}
+
+			var cacheValue = TryExecute((d, k) => d.StringGet(k), key);
+
+			value = cacheValue;
+
+			return cacheValue.HasValue;
+		}
+
+		public void Set(string key, string value)
+		{
+			if (string.IsNullOrEmpty(key))
+			{
+				throw new ArgumentNullException("key");
+			}
+
+			if (value == null)
+			{
+				throw new ArgumentNullException("value");
+			}
+
+			TryExecute((d, k) => d.StringSet(k, value), key);
+		}
+
+		public void Remove(string key)
+		{
+			if (string.IsNullOrEmpty(key))
+			{
+				throw new ArgumentNullException("key");
+			}
+
+			TryExecute((d, k) => d.KeyDelete(k), key);
+		}
+
+		public void Clear()
+		{
+			TryExecute((d, k) =>
+			{
+				var endpoints = d.Multiplexer.GetEndPoints();
+
+				if (endpoints != null && endpoints.Length > 0)
+				{
+					var server = d.Multiplexer.GetServer(endpoints[0]);
+
+					if (server != null)
+					{
+						var allKeys = server.Keys(d.Database, k).ToArray();
+
+						d.KeyDelete(allKeys);
+					}
+				}
+
+				return true;
+			}, "*");
+		}
+
+		public void Dispose()
+		{
+			if (_database.IsValueCreated)
+			{
+				var connection = _database.Value.Multiplexer;
+				connection.Dispose();
+			}
+		}
+
+
+		private static IDatabase CreateDatabase(string connectionString)
+		{
+			var connection = ConnectionMultiplexer.Connect(connectionString);
+			var database = connection.GetDatabase();
+			return database;
+		}
+
+		private T TryExecute<T>(Func<IDatabase, string, T> action, string key)
+		{
+			var wrappedKey = key.WrapCacheKey(_name);
+
+			return CachingExtensions.TryExecute(() => action(_database.Value, wrappedKey));
+		}
+	}
+}
