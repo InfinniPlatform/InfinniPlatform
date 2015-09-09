3ae15ca65e6a1a14cecd4781d7190210855bd47b
AlexMAS <as.mezhov@gmail.com>
2015-09-09 10:36:22 +0500

    Add InfinniPlatform.Caching

---
+++ InfinniPlatform.Caching.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿// 1
+// 0
+// 0
+// 0
+// e4d7c70c-f088-46df-8418-5cbd25792cac
+// 7E6565AA73619B5F31D6A17CEA72E13F
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InfinniPlatform.Caching.Tests")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Infinnity Solutions")]
+[assembly: AssemblyProduct("InfinniPlatform")]
+[assembly: AssemblyCopyright("Copyright © Infinnity Solutions 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e4d7c70c-f088-46df-8418-5cbd25792cac")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDescription("InfinniPlatform.Caching.Tests")]
---
+++ InfinniPlatform.Caching/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿// 1
+// 0
+// 0
+// 0
+// a609e979-0e19-4709-9fa4-0dff5ee21215
+// BA21EDC5BE56503193E6C3664F5D67E5
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InfinniPlatform.Caching")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Infinnity Solutions")]
+[assembly: AssemblyProduct("InfinniPlatform")]
+[assembly: AssemblyCopyright("Copyright © Infinnity Solutions 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a609e979-0e19-4709-9fa4-0dff5ee21215")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("InfinniPlatform.Caching.Tests")]
+[assembly: AssemblyDescription("InfinniPlatform.Caching")]
