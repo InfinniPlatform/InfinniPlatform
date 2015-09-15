da51f0e3733e7f1b6db24161876fbd2febf3d148
Sergey Pevnev <s.pevnev@infinnitysolutions.com>
2015-09-15 15:23:17 +0500

    Fix InfinniPlatform.UserInteface.exe

--- InfinniPlatform.UserInterface/ViewBuilders/Designers/ConfigSelector/ConfigSelectorElementBuilder.cs
+++ InfinniPlatform.UserInterface/ViewBuilders/Designers/ConfigSelector/ConfigSelectorElementBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+
 using InfinniPlatform.UserInterface.Services.Metadata;
 using InfinniPlatform.UserInterface.ViewBuilders.Data;
 using InfinniPlatform.UserInterface.ViewBuilders.Elements;
@@ -6,41 +7,43 @@ using InfinniPlatform.UserInterface.ViewBuilders.Views;
 
 namespace InfinniPlatform.UserInterface.ViewBuilders.Designers.ConfigSelector
 {
-    internal sealed class ConfigSelectorElementBuilder : IObjectBuilder
-    {
-        private readonly string _server;
-        private readonly int _port;
-        private readonly string _routeVersion;
+	internal sealed class ConfigSelectorElementBuilder : IObjectBuilder
+	{
+		public ConfigSelectorElementBuilder(string server, int port, string routeVersion)
+		{
+			_server = server;
+			_port = port;
+			_routeVersion = routeVersion;
+		}
 
-        public ConfigSelectorElementBuilder(string server, int port, string routeVersion)
-        {
-            _server = server;
-            _port = port;
-            _routeVersion = routeVersion;
-        }
+		private readonly int _port;
+		private readonly string _routeVersion;
+		private readonly string _server;
+		private dynamic _version;
 
-        public object Build(ObjectBuilderContext context, View parent, dynamic metadata)
-        {
-            var element = new ConfigSelectorElement(parent);
-            element.ApplyElementMeatadata((object) metadata);
-            element.SetConfigurationsFunc(GetConfigurations(context.AppView.GetContext().Version));
+		public object Build(ObjectBuilderContext context, View parent, dynamic metadata)
+		{
+			var element = new ConfigSelectorElement(parent);
+			element.ApplyElementMeatadata((object)metadata);
+			_version = context.AppView.GetContext().Version;
+			element.SetConfigurationsFunc(GetConfigurations);
 
-            // Привязка к источнику данных
+			// Привязка к источнику данных
 
-            IElementDataBinding valueBinding = context.Build(parent, metadata.Value);
+			IElementDataBinding valueBinding = context.Build(parent, metadata.Value);
 
-            if (valueBinding != null)
-            {
-                valueBinding.OnPropertyValueChanged += (c, a) => element.SetValue(a.Value);
-                element.OnValueChanged += (c, a) => valueBinding.SetPropertyValue(a.Value);
-            }
+			if (valueBinding != null)
+			{
+				valueBinding.OnPropertyValueChanged += (c, a) => element.SetValue(a.Value);
+				element.OnValueChanged += (c, a) => valueBinding.SetPropertyValue(a.Value);
+			}
 
-            return element;
-        }
+			return element;
+		}
 
-        private IEnumerable GetConfigurations(string version)
-        {
-            return new ConfigurationMetadataService(version, _server, _port, _routeVersion).GetItems();
-        }
-    }
+		IEnumerable GetConfigurations()
+		{
+			return new ConfigurationMetadataService(_version, _server, _port, _routeVersion).GetItems();
+		}
+	}
 }
