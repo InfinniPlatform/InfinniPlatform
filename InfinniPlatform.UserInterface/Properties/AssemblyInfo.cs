bb46434b477496ff1cd83b66b4816b3cab0cc1a5
Sergey Pevnev <s.pevnev@infinnitysolutions.com>
2015-09-15 15:32:46 +0500

    Fix InfinniPlatform.UserInteface.exe
    Conflicts:
    	InfinniPlatform.UserInterface/Properties/AssemblyInfo.cs

--- InfinniPlatform.UserInterface/Properties/AssemblyInfo.cs
+++ InfinniPlatform.UserInterface/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 37
+// 38
 // bb6519e1-674f-4bc5-ba40-5de77e5f0ecc
-// 79CD0E88C34CDE25AAF2A9B4EFBD3F7B
+// 4B1165881F273D2B1D0248D8A5FD529A
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("bb6519e1-674f-4bc5-ba40-5de77e5f0ecc")]
-[assembly: AssemblyVersion("1.0.0.37")]
-[assembly: AssemblyFileVersion("1.0.0.37")]
+[assembly: AssemblyVersion("1.0.0.38")]
+[assembly: AssemblyFileVersion("1.0.0.38")]
 [assembly: InternalsVisibleTo("InfinniPlatform.UserInterface.Tests")]
 [assembly: AssemblyDescription("InfinniPlatform.UserInterface")]
