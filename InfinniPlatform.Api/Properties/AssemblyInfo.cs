75c97e5c70d80ea4d7b859e64341da514f912bb1
AlexMAS <as.mezhov@gmail.com>
2015-09-25 13:29:08 +0500

    Update RestSharp

--- InfinniPlatform.Api/Properties/AssemblyInfo.cs
+++ InfinniPlatform.Api/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 64
+// 65
 // e0ecaf6a-c2c2-4c8a-be1e-af6444fe8342
-// 2FCDF5B174DEFFEBEF6772B597F2B8CF
+// 420D6BAB25D7D2AAFD4CAA93A12DF521
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e0ecaf6a-c2c2-4c8a-be1e-af6444fe8342")]
-[assembly: AssemblyVersion("1.0.0.64")]
-[assembly: AssemblyFileVersion("1.0.0.64")]
+[assembly: AssemblyVersion("1.0.0.65")]
+[assembly: AssemblyFileVersion("1.0.0.65")]
 [assembly: InternalsVisibleTo("InfinniPlatform.Api.Tests")]
 [assembly: AssemblyDescription("InfinniPlatform SDK for .NET")]
