0cf56f55d92f91ecb31c66fe8ef4e20e8f24425c
Sergey Pevnev <s.pevnev@infinnitysolutions.com>
2015-09-29 15:17:22 +0500

    Fix InfinniSignInApi
    Conflicts:
    	InfinniPlatform.Api/Properties/AssemblyInfo.cs
    	InfinniPlatform.Api/Properties/Package.nuspec

--- InfinniPlatform.Sdk/Api/InfinniSignInApi.cs
+++ InfinniPlatform.Sdk/Api/InfinniSignInApi.cs
@@ -1,112 +1,91 @@
 ﻿using System;
 using System.Net;
+using System.Net.Http;
+using System.Text;
+
 using InfinniPlatform.Sdk.ApiContracts;
 using InfinniPlatform.Sdk.Properties;
 
+using Newtonsoft.Json;
+
 namespace InfinniPlatform.Sdk.Api
 {
-    /// <summary>
-    ///   API для аутентификации пользователей
-    /// </summary>
-    public sealed class InfinniSignInApi : BaseApi, ISignInApi
-    {
+	/// <summary>
+	/// API для аутентификации пользователей.
+	/// </summary>
+	public sealed class InfinniSignInApi : BaseApi, ISignInApi
+	{
+		public InfinniSignInApi(string server, string port, string route = "")
+			: base(server, port, route)
+		{
+			var cookieContainer = new CookieContainer();
+			var clientHandler = new HttpClientHandler { CookieContainer = cookieContainer };
+			var clientBaseAddress = new Uri(string.Format("http://{0}:{1}/Auth/", server, port));
+			var client = new HttpClient(clientHandler) { BaseAddress = clientBaseAddress };
 
-        public InfinniSignInApi(string server, string port, string route)
-            : base(server, port, route)
-        {
-        }
+			CookieContainer = cookieContainer;
 
-        /// <summary>
-        ///   Зарегистрироваться с использованием внутреннего хранилища пользователей
-        /// </summary>
-        /// <param name="userName">Имя пользователя</param>
-        /// <param name="password">Пароль пользователя</param>
-        /// <param name="remember">Запомнить пользователя</param>
-        /// <returns>Результат попытки регистрации</returns>
-        public dynamic SignInInternal(string userName, string password, bool remember)
-        {
-            var restQueryExecutor = new RequestExecutor(CookieContainer);
-
-            var requestBody = new
-            {
-                UserName = userName,
-                Password = password,
-                Remember = remember
-            };
-
-            var response = restQueryExecutor.QueryPost(RouteBuilder.BuildRestRoutingUrlSignIn(),requestBody);
-
-            var cookieContainer = ProcessAsObjectResult(response,
-                            string.Format(Resources.UnableToSignInUser, response.GetErrorContent()));   
-
-            CreateCookieContainer(cookieContainer);
-
-            return cookieContainer;
-        }
-
-        /// <summary>
-        ///   Сменить пароль пользователя
-        /// </summary>
-        /// <param name="userName">Имя пользователя</param>
-        /// <param name="oldPassword">Старый пароль пользователя</param>
-        /// <param name="newPassword">Новый пароль пользователя</param>
-        /// <returns>Признак успешной смены пароля пользователя</returns>
-        public dynamic ChangePassword(string userName, string oldPassword, string newPassword)
-        {
-            var restQueryExecutor = new RequestExecutor(CookieContainer);
-
-            var requestBody = new
-            {
-                UserName = userName,
-                OldPassword = oldPassword,
-                NewPassword = newPassword
-            };
-
-            var response = restQueryExecutor.QueryPost(RouteBuilder.BuildRestRoutingChangePassword(), requestBody);
-
-            return ProcessAsObjectResult(response,
-                            string.Format(Resources.UnableToChangePasswordUser, response.GetErrorContent()));   
-
-        }
-
-        /// <summary>
-        ///   Выйти из системы
-        /// </summary>
-        /// <returns>Признак успешного выхода из системы</returns>
-        public dynamic SignOut()
-        {
-            var restQueryExecutor = new RequestExecutor(CookieContainer);
-
-            var response = restQueryExecutor.QueryPost(RouteBuilder.BuildRestRoutingUrlSignOut());
-
-            return ProcessAsObjectResult(response,
-                            string.Format(Resources.UnableToSignOutUser, response.GetErrorContent())); 
-
-        }
-
-        private void CreateCookieContainer(dynamic result)
-        {
-            if (result != null && result.ResponseCookies != null && result.UserInfo != null)
-            {
-                CookieContainer = new CookieContainer();
-
-                foreach (var cookie in result.ResponseCookies)
-                {
-                    var responseCookie = new Cookie()
-                    {
-                        Name = cookie.Name.ToString(),
-                        Value = cookie.Value.ToString(),
-						//TODO В куки попадал localhost, вместо реального адреса сервера.
-						Domain = Server,
-                        Expires = Convert.ToDateTime(cookie.Expires),
-                        Path = cookie.Path.ToString(),
-                        Discard = Convert.ToBoolean(cookie.Discard)
-                    };
-                    CookieContainer.Add(responseCookie);
-                }
-            }
-        }
+			_client = client;
+		}
 
 
-    }
+		private readonly HttpClient _client;
+
+
+		/// <summary>
+		/// Осуществляет вход пользователя в систему через внутренний провайдер.
+		/// </summary>
+		/// <param name="userName">Имя пользователя.</param>
+		/// <param name="password">Пароль пользователя.</param>
+		/// <param name="remember">Запомнить пользователя.</param>
+		public object SignInInternal(string userName, string password, bool remember = true)
+		{
+			return ExecutePostRequest("SignInInternal", new { UserName = userName, Password = password, Remember = remember });
+		}
+
+		/// <summary>
+		/// Изменяет пароль текущего пользователя.
+		/// </summary>
+		/// <param name="oldPassword">Старый пароль пользователя.</param>
+		/// <param name="newPassword">Новый пароль пользователя.</param>
+		public object ChangePassword(string oldPassword, string newPassword)
+		{
+			return ExecutePostRequest("ChangePassword", new { OldPassword = oldPassword, NewPassword = newPassword });
+		}
+
+		/// <summary>
+		/// Осуществляет выход пользователя из системы.
+		/// </summary>
+		public object SignOut()
+		{
+			return ExecutePostRequest("SignOut");
+		}
+
+
+		private string ExecutePostRequest(string requestUrl, object requestBody = null)
+		{
+			var jsonRequestBody = (requestBody != null) ? SerializeObjectToJson(requestBody) : string.Empty;
+			var jsonRequestContent = new StringContent(jsonRequestBody, Encoding.UTF8, "application/json");
+
+			var response = _client.PostAsync(new Uri(requestUrl, UriKind.Relative), jsonRequestContent).Result;
+			var content = response.Content.ReadAsStringAsync().Result;
+
+			if (!response.IsSuccessStatusCode)
+			{
+				throw new InvalidOperationException(string.Format(Resources.CannotProcessPostRequest, response.StatusCode, content));
+			}
+
+			return content;
+		}
+
+		private static string SerializeObjectToJson(object target)
+		{
+			if (target != null)
+			{
+				return JsonConvert.SerializeObject(target);
+			}
+
+			return null;
+		}
+	}
 }
--- InfinniPlatform.Sdk/ApiContracts/ISignInApi.cs
+++ InfinniPlatform.Sdk/ApiContracts/ISignInApi.cs
@@ -1,35 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace InfinniPlatform.Sdk.ApiContracts
+﻿namespace InfinniPlatform.Sdk.ApiContracts
 {
-    public interface ISignInApi
-    {
-        /// <summary>
-        ///   Зарегистрироваться с использованием внутреннего хранилища пользователей
-        /// </summary>
-        /// <param name="userName">Имя пользователя</param>
-        /// <param name="password">Пароль пользователя</param>
-        /// <param name="remember">Запомнить пользователя</param>
-        /// <returns>Результат попытки регистрации</returns>
-        dynamic SignInInternal(string userName, string password, bool remember);
+	/// <summary>
+	/// API для аутентификации пользователей.
+	/// </summary>
+	public interface ISignInApi
+	{
+		/// <summary>
+		/// Осуществляет вход пользователя в систему через внутренний провайдер.
+		/// </summary>
+		/// <param name="userName">Имя пользователя.</param>
+		/// <param name="password">Пароль пользователя.</param>
+		/// <param name="remember">Запомнить пользователя.</param>
+		object SignInInternal(string userName, string password, bool remember = true);
 
-        /// <summary>
-        ///   Сменить пароль пользователя
-        /// </summary>
-        /// <param name="userName">Имя пользователя</param>
-        /// <param name="oldPassword">Старый пароль пользователя</param>
-        /// <param name="newPassword">Новый пароль пользователя</param>
-        /// <returns>Признак успешной смены пароля пользователя</returns>
-        dynamic ChangePassword(string userName, string oldPassword, string newPassword);
+		/// <summary>
+		/// Изменяет пароль текущего пользователя.
+		/// </summary>
+		/// <param name="oldPassword">Старый пароль пользователя.</param>
+		/// <param name="newPassword">Новый пароль пользователя.</param>
+		object ChangePassword(string oldPassword, string newPassword);
 
-        /// <summary>
-        ///   Выйти из системы
-        /// </summary>
-        /// <returns>Признак успешного выхода из системы</returns>
-        dynamic SignOut();
-    }
+		/// <summary>
+		/// Осуществляет выход пользователя из системы.
+		/// </summary>
+		object SignOut();
+	}
 }
