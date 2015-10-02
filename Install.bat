f31d7376748322b3f463ef2686de8f979dd8c21f
Sergey Pevnev <s.pevnev@infinnitysolutions.com>
2015-10-02 16:05:43 +0500

    ~ Install.bat fix

--- Install.bat
+++ Install.bat
@@ -1,12 +1,6 @@
 ::Copy metadata
 xcopy SystemConfiguration_1.0.0.0\* Assemblies\SystemConfiguration_1.0.0.0\ /s /y
 
-::Start ServiceHost
-start /d ..\InfinniPlatform\Assemblies\ InfinniPlatform.ServiceHost.exe
-
-::Wait 5 sec till ServiceHost start
-timeout 5
-
 ::Upload assemblies and metadata
 pushd Assemblies
 InfinniPlatform.Utils.exe upload SystemConfiguration_1.0.0.0 withMetadata
