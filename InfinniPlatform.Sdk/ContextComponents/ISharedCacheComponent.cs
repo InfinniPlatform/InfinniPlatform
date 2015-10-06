0bcd4519a187bec733e0b6682d4779cadd70f89f
Sergey Pevnev <s.pevnev@infinnitysolutions.com>
2015-10-06 17:03:11 +0500

    MC-3743: Подключить разделяемый кэш #resolved

--- InfinniPlatform.Sdk/ContextComponents/ISharedCacheComponent.cs
+++ InfinniPlatform.Sdk/ContextComponents/ISharedCacheComponent.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InfinniPlatform.Sdk.ContextComponents
 {
-    public interface ISharedCacheComponent
-    {
-        object Get(string key);
-
-        void Set(string key, object item);
-        void Lock();
-        void Unlock();
-    }
+	[Obsolete]
+	public interface ISharedCacheComponent
+	{
+		object Get(string key);
+		void Set(string key, object item);
+		void Lock();
+		void Unlock();
+	}
 }
