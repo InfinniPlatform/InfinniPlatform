577de76e45ecebb20ed837e28af218f4f6ebe4fb
AlexMAS <as.mezhov@gmail.com>
2015-10-08 09:42:51 +0500

    Add metadata folder to SDK tests

--- Gameshop_1.0.0.0/Download.cmd
+++
@@ -1,3 +0,0 @@
-pushd ..\Assemblies
-InfinniPlatform.Utils.exe download GameshopDownload Gameshop 1.0.0.0  1.0.0.0
-popd
--- Gameshop_1.0.0.0/Install.cmd
+++
@@ -1,4 +0,0 @@
-xcopy ..\Gameshop_1.0.0.0\* ..\Assemblies\Gameshop_1.0.0.0\ /s /y /r
-pushd ..\Assemblies
-InfinniPlatform.Utils.exe upload Gameshop_1.0.0.0 withMetadata
-popd
