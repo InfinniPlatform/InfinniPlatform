16c1cef288b4631b7dc561bae01503cf201b6395
AlexMAS <as.mezhov@gmail.com>
2015-10-08 14:26:02 +0500

    Merge

--- InfinniPlatform.Core/Properties/AssemblyInfo.cs
+++ InfinniPlatform.Core/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 38
+// 39
 // a4a50401-f0a2-4f0e-b341-ca1f2f1e92c4
-// 344371EEB41CBFB621131579D8B2F050
+// 0774847F5845DDB74DFD8EDBE7D0581B
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a4a50401-f0a2-4f0e-b341-ca1f2f1e92c4")]
-[assembly: AssemblyVersion("1.0.0.38")]
-[assembly: AssemblyFileVersion("1.0.0.38")]
+[assembly: AssemblyVersion("1.0.0.39")]
+[assembly: AssemblyFileVersion("1.0.0.39")]
 [assembly: InternalsVisibleTo("InfinniPlatform.Core.Tests")]
 [assembly: AssemblyDescription("InfinniPlatform.Core")]
