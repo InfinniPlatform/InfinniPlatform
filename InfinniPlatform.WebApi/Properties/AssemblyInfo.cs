06fcab0184d7a54a379a6e6279ae0f778a4faa56
AlexMAS <as.mezhov@gmail.com>
2015-10-09 14:45:35 +0500

    Merge

--- InfinniPlatform.WebApi/Properties/AssemblyInfo.cs
+++ InfinniPlatform.WebApi/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 26
+// 27
 // 87a52fdc-6487-4183-ba72-0dad23ce43e7
-// 354246896458F63A64051858082E14E3
+// B954A6678FF587F0ECD2532BC1A01DAF
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("87a52fdc-6487-4183-ba72-0dad23ce43e7")]
-[assembly: AssemblyVersion("1.0.0.26")]
-[assembly: AssemblyFileVersion("1.0.0.26")]
+[assembly: AssemblyVersion("1.0.0.27")]
+[assembly: AssemblyFileVersion("1.0.0.27")]
 [assembly: InternalsVisibleTo("InfinniPlatform.WebApi.Tests")]
 [assembly: AssemblyDescription("InfinniPlatform.WebApi")]
