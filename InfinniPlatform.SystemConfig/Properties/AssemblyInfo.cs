3f5ee48b460e4ef0e3e2a82aa15bf9076640c883
AlexMAS <as.mezhov@gmail.com>
2015-10-23 12:34:57 +0500

    Ignore VersionApiTest because it is obsolete

--- InfinniPlatform.SystemConfig/Properties/AssemblyInfo.cs
+++ InfinniPlatform.SystemConfig/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿// 1
 // 0
 // 0
-// 65
+// 68
 // 3dbb67e9-cec8-4cb9-979d-256ebb464baf
 // 535838D69C73A9C9E24B14BF8D21FD39
 using System.Reflection;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3dbb67e9-cec8-4cb9-979d-256ebb464baf")]
-[assembly: AssemblyVersion("1.0.0.65")]
-[assembly: AssemblyFileVersion("1.0.0.65")]
+[assembly: AssemblyVersion("1.0.0.68")]
+[assembly: AssemblyFileVersion("1.0.0.68")]
 [assembly: InternalsVisibleTo("InfinniPlatform.SystemConfig.Tests")]
 [assembly: AssemblyDescription("InfinniPlatform.SystemConfig")]
