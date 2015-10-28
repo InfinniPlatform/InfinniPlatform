e039d0ee277d4232f63ae3ee2efbeca7c802d2af
Sergey Pevnev <s.pevnev@infinnitysolutions.com>
2015-10-28 10:07:32 +0500

    PrintViewMetadataService restored

--- InfinniPlatform.UserInterface/Properties/AssemblyInfo.cs
+++ InfinniPlatform.UserInterface/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 213
+// 216
 // bb6519e1-674f-4bc5-ba40-5de77e5f0ecc
-// 6EF1FD885DECFD7474C989E6973900B2
+// C78AD0F0575E4395696591FBD170274A
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("bb6519e1-674f-4bc5-ba40-5de77e5f0ecc")]
-[assembly: AssemblyVersion("1.0.0.213")]
-[assembly: AssemblyFileVersion("1.0.0.213")]
+[assembly: AssemblyVersion("1.0.0.216")]
+[assembly: AssemblyFileVersion("1.0.0.216")]
 [assembly: InternalsVisibleTo("InfinniPlatform.UserInterface.Tests")]
 [assembly: AssemblyDescription("InfinniPlatform.UserInterface")]
--- InfinniPlatform.UserInterface/Services/Metadata/PrintViewMetadataService.cs
+++ InfinniPlatform.UserInterface/Services/Metadata/PrintViewMetadataService.cs
@@ -1,54 +1,88 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-using InfinniPlatform.Sdk.Api;
+using InfinniPlatform.Api.Serialization;
+using InfinniPlatform.Sdk.Dynamic;
 
 namespace InfinniPlatform.UserInterface.Services.Metadata
 {
-    /// <summary>
-    ///     Сервис для работы с метаданными печатных представлений.
-    /// </summary>
-    internal sealed class PrintViewMetadataService : BaseMetadataService
-    {
-        private readonly string _configId;
-        private readonly string _documentId;
-        private InfinniMetadataApi _metadataApi;
+	/// <summary>
+	/// Сервис для работы с метаданными печатных представлений.
+	/// </summary>
+	internal sealed class PrintViewMetadataService : BaseMetadataService
+	{
+		public PrintViewMetadataService(string version, string configId, string documentId, string server, int port, string route)
+			: base(version, server, port, route)
+		{
+			_configId = configId;
+			_documentId = documentId;
+		}
 
-        public PrintViewMetadataService(string version, string configId, string documentId, string server, int port, string route)
-            : base(version, server, port, route)
-        {
-            _configId = configId;
-            _documentId = documentId;
-            _metadataApi = new InfinniMetadataApi(server, port.ToString(), route);
-        }
+		private readonly string _configId;
+		private readonly string _documentId;
 
-        public string ConfigId
-        {
-            get { return _configId; }
-        }
+		public string ConfigId
+		{
+			get { return _configId; }
+		}
 
-        public override object CreateItem()
-        {
-            return _metadataApi.CreatePrintView(Version, ConfigId,_documentId);
-        }
+		public override object CreateItem()
+		{
+			dynamic printView = new DynamicWrapper();
 
-        public override void ReplaceItem(dynamic item)
-        {
-            _metadataApi.UpdatePrintView(item, Version,ConfigId,_documentId);
-        }
+			printView.Id = Guid.NewGuid().ToString();
+			printView.Name = string.Empty;
+			printView.Caption = string.Empty;
+			printView.Description = string.Empty;
 
-        public override void DeleteItem(string itemId)
-        {
-            _metadataApi.DeletePrintView(Version, ConfigId, _documentId, itemId);
-        }
+			return printView;
+		}
 
-        public override object GetItem(string itemId)
-        {
-            return _metadataApi.GetPrintView(Version, ConfigId, _documentId, itemId);
-        }
+		public override void ReplaceItem(dynamic item)
+		{
+			string filePath;
+			var serializedItem = JsonObjectSerializer.Formated.Serialize(item);
 
-        public override IEnumerable<object> GetItems()
-        {
-            return _metadataApi.GetPrintViewItems(Version, ConfigId, _documentId);
-        }
-    }
+			//TODO Wrapper for PackageMetadataLoader.Configurations
+			if (PackageMetadataLoader.Configurations[_configId].Documents[_documentId].Views.ContainsKey(item.Name))
+			{
+				dynamic oldView = PackageMetadataLoader.Configurations[_configId].Documents[_documentId].PrintViews[item.Name];
+				filePath = oldView.FilePath;
+			}
+			else
+			{
+				filePath = Path.Combine(Path.GetDirectoryName(PackageMetadataLoader.Configurations[_configId].FilePath),
+										"Documents",
+										_documentId,
+										"PrintViews",
+										string.Concat(item.Name, ".json"));
+			}
+
+			File.WriteAllBytes(filePath, serializedItem);
+
+			PackageMetadataLoader.UpdateCache();
+		}
+
+		public override void DeleteItem(string itemId)
+		{
+			dynamic document = PackageMetadataLoader.Configurations[_configId].Documents[_documentId].PrintViews[itemId];
+
+			File.Delete(document.FilePath);
+
+			PackageMetadataLoader.UpdateCache();
+		}
+
+		public override object GetItem(string itemId)
+		{
+			return PackageMetadataLoader.Configurations[_configId].Documents[_documentId].PrintViews[itemId].Content;
+		}
+
+		public override IEnumerable<object> GetItems()
+		{
+			Dictionary<string, dynamic> printViews = PackageMetadataLoader.Configurations[_configId].Documents[_documentId].PrintViews;
+			return printViews.Values.Select(o => o.Content);
+		}
+	}
 }
