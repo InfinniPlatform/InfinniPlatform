99fafd742bc1a7889ebb9fcfe7be1adb28b80464
AlexMAS <as.mezhov@gmail.com>
2015-11-02 09:27:41 +0500

    MC-3710: Add all aggregation buckets (size=0) when Terms Aggregation #progress

--- InfinniPlatform.Index.ElasticSearch/Properties/AssemblyInfo.cs
+++ InfinniPlatform.Index.ElasticSearch/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 69
+// 70
 // 2d773c40-1f61-4b34-8917-14fd87bcd6ac
-// E49D4916068ED79D3E905DF29F5E1D81
+// B7ED9B2BACB56B14FD37D3D1F06EB3EF
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2d773c40-1f61-4b34-8917-14fd87bcd6ac")]
-[assembly: AssemblyVersion("1.0.0.69")]
-[assembly: AssemblyFileVersion("1.0.0.69")]
+[assembly: AssemblyVersion("1.0.0.70")]
+[assembly: AssemblyFileVersion("1.0.0.70")]
 [assembly: InternalsVisibleTo("InfinniPlatform.Index.ElasticSearch.Tests")]
 [assembly: AssemblyDescription("InfinniPlatform.Index.ElasticSearch")]
