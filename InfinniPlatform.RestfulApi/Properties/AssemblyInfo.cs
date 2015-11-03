c3b47f998cee6eebe8f0c65e52ea63ec6fdd1c60
Sergey Pevnev <s.pevnev@infinnitysolutions.com>
2015-11-03 15:48:10 +0500

    ActionUnitRemoveUser null reference exception fix

--- InfinniPlatform.RestfulApi/Properties/AssemblyInfo.cs
+++ InfinniPlatform.RestfulApi/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 80
+// 81
 // fa6e26d2-0281-45f1-a6d3-4638ce94a6e2
-// 10C09DBD525A8D44F461A45C7DE9FEA4
+// 5543EAADE40FEB337970B34663EF4203
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("fa6e26d2-0281-45f1-a6d3-4638ce94a6e2")]
-[assembly: AssemblyVersion("1.0.0.80")]
-[assembly: AssemblyFileVersion("1.0.0.80")]
+[assembly: AssemblyVersion("1.0.0.81")]
+[assembly: AssemblyFileVersion("1.0.0.81")]
 [assembly: InternalsVisibleTo("InfinniPlatform.RestfulApi.Tests")]
 [assembly: AssemblyDescription("InfinniPlatform.RestfulApi")]
