c407d8457b9e98fbda12a4e07a815fcb254568b7
Sergey Pevnev <s.pevnev@infinnitysolutions.com>
2015-11-19 11:19:21 +0500

    Remove obsolete tests

--- InfinniPlatform.WebApi.Tests/Properties/AssemblyInfo.cs
+++ InfinniPlatform.WebApi.Tests/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 28
+// 30
 // 741b0a83-c0c1-4743-815e-f4beb8f00931
-// 857E65799A7BDC3C59B14C4FFDA9E726
+// 0AB537E023668116C83140BB256B4F21
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("741b0a83-c0c1-4743-815e-f4beb8f00931")]
-[assembly: AssemblyVersion("1.0.0.28")]
-[assembly: AssemblyFileVersion("1.0.0.28")]
+[assembly: AssemblyVersion("1.0.0.30")]
+[assembly: AssemblyFileVersion("1.0.0.30")]
 
 [assembly: AssemblyDescription("InfinniPlatform.WebApi.Tests")]
