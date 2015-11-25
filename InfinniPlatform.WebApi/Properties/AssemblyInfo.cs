1efe36189b2a76f85b04448fd0f0b7ccf9004456
AlexMAS <as.mezhov@gmail.com>
2015-11-25 21:18:42 +0500

    MC-3905: Fix after merge

--- InfinniPlatform.WebApi/Properties/AssemblyInfo.cs
+++ InfinniPlatform.WebApi/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 84
+// 85
 // 87a52fdc-6487-4183-ba72-0dad23ce43e7
-// B7D15CC5250F56DD46DD40715A669CDF
+// 7A5214ED4B8A434E13A330F2A961CFA7
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("87a52fdc-6487-4183-ba72-0dad23ce43e7")]
-[assembly: AssemblyVersion("1.0.0.84")]
-[assembly: AssemblyFileVersion("1.0.0.84")]
+[assembly: AssemblyVersion("1.0.0.85")]
+[assembly: AssemblyFileVersion("1.0.0.85")]
 [assembly: InternalsVisibleTo("InfinniPlatform.WebApi.Tests")]
 [assembly: AssemblyDescription("InfinniPlatform.WebApi")]
