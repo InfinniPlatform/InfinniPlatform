242091aeace7cd4d8e87b4962488f695835868ff
Sergey Pevnev <s.pevnev@infinnitysolutions.com>
2015-11-26 10:55:47 +0500

    Designer fix: delete view

--- InfinniPlatform.UserInterface/Properties/AssemblyInfo.cs
+++ InfinniPlatform.UserInterface/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 281
+// 282
 // bb6519e1-674f-4bc5-ba40-5de77e5f0ecc
-// 55D1B69EE6F2812F078DB557B184B1EA
+// EF66E74528B545B4F2E70E216BB94C3F
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("bb6519e1-674f-4bc5-ba40-5de77e5f0ecc")]
-[assembly: AssemblyVersion("1.0.0.281")]
-[assembly: AssemblyFileVersion("1.0.0.281")]
+[assembly: AssemblyVersion("1.0.0.282")]
+[assembly: AssemblyFileVersion("1.0.0.282")]
 [assembly: InternalsVisibleTo("InfinniPlatform.UserInterface.Tests")]
 [assembly: AssemblyDescription("InfinniPlatform.UserInterface")]
