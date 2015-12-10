fda6e054481b2a17d503e4205b87023537c1c5b9
Sergey Pevnev <larockarocka@hotmail.com>
2015-12-10 23:17:27 +0500

    Added commit hash to description

--- Files/Packaging/SetVersion.ps1
+++
@@ -1,9 +0,0 @@
-Param ([string]$Version)
-
-$Stub = "{INFINNI_PLATFORM_VERSION}"
-
-(Get-Content ".\Templates\InfinniPlatform.Template.nuspec") | 
-Foreach-Object {$_ -replace $Stub, $Version} | 
-Set-Content ".\InfinniPlatform.nuspec"
-
-Write-Host "InfinniPlatform $Version is set."    
