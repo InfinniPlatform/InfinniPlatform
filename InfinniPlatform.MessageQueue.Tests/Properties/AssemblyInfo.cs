ae3519ef78d400c0bdfad2551474a628db95b148
AlexMAS <as.mezhov@gmail.com>
2015-12-15 09:46:03 +0500

    PC-2396: Ignore ShouldDeliverAllMessages test because it is Windows test only

--- InfinniPlatform.MessageQueue.Tests/Properties/AssemblyInfo.cs
+++ InfinniPlatform.MessageQueue.Tests/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿// 1
 // 0
 // 0
-// 11
+// 13
 // 82974aed-39ef-4a34-98e1-3b8e909166a9
-// 25BE7E2B887025A057FA6A511DCA85D1
+// 2AFC699C5A3D86093B1869AE66521174
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("82974aed-39ef-4a34-98e1-3b8e909166a9")]
-[assembly: AssemblyVersion("1.0.0.11")]
-[assembly: AssemblyFileVersion("1.0.0.11")]
+[assembly: AssemblyVersion("1.0.0.13")]
+[assembly: AssemblyFileVersion("1.0.0.13")]
 
 [assembly: AssemblyDescription("InfinniPlatform.MessageQueue.Tests")]
