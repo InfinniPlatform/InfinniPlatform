a0863dbc082fabdd491561cb7071558c36b2a632
AlexMAS <as.mezhov@gmail.com>
2015-12-22 14:05:35 +0500

    Remove unused ActionUnits for SetDocument

--- InfinniPlatform.RestfulApi/DefaultProcessUnits/ValidationUnitSetDocumentError.cs
+++ InfinniPlatform.RestfulApi/DefaultProcessUnits/ValidationUnitSetDocumentError.cs
@@ -1,110 +1,76 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 
 using InfinniPlatform.Api.ContextTypes.ContextImpl;
 using InfinniPlatform.Api.Metadata;
-using InfinniPlatform.Api.Properties;
-using InfinniPlatform.Api.Validation;
-using InfinniPlatform.Logging;
 using InfinniPlatform.Sdk.ContextComponents;
 using InfinniPlatform.Sdk.Contracts;
-using InfinniPlatform.Sdk.Environment;
-using InfinniPlatform.Sdk.Environment.Log;
-using InfinniPlatform.Sdk.Environment.Metadata;
 using InfinniPlatform.Sdk.Environment.Validations;
 using InfinniPlatform.Sdk.Global;
 
 namespace InfinniPlatform.RestfulApi.DefaultProcessUnits
 {
-    /// <summary>
-    ///     Валидатор для применения правил проверки документа, описанных в его метаданных
-    /// </summary>
     public sealed class ValidationUnitSetDocumentError
     {
+        public ValidationUnitSetDocumentError(IMetadataComponent metadataComponent, IScriptRunnerComponent scriptRunnerComponent, ICustomServiceGlobalContext customServiceGlobalContext)
+        {
+            _metadataComponent = metadataComponent;
+            _scriptRunnerComponent = scriptRunnerComponent;
+            _customServiceGlobalContext = customServiceGlobalContext;
+        }
+
+
+        private readonly ICustomServiceGlobalContext _customServiceGlobalContext;
+        private readonly IMetadataComponent _metadataComponent;
+        private readonly IScriptRunnerComponent _scriptRunnerComponent;
+
+
         public void Action(IApplyContext target)
         {
-            dynamic validationMessage = target.ValidationMessage;
-
-            dynamic defaultBusinessProcess = null;
-
-            //TODO Игнорировать системные конфигурации при валидации. Пока непонятно, как переделать
-            if (target.Item.Configuration.ToLowerInvariant() != "systemconfig" &&
-                target.Item.Configuration.ToLowerInvariant() != "update" &&
-                target.Item.Configuration.ToLowerInvariant() != "restfulapi")
-            {
-                //ищем метаданные бизнес-процесса по умолчанию документа 
-                defaultBusinessProcess =
-                    target.Context.GetComponent<IMetadataComponent>()
-                          .GetMetadata(target.Item.Configuration, target.Item.Metadata,
-                              MetadataType.Process, "Default");
-            }
-
-            if (defaultBusinessProcess == null || defaultBusinessProcess.Transitions.Count == 0)
-            {
-                target.ValidationMessage = validationMessage;
-                return;
-            }
+            string configuration = target.Item.Configuration;
+            string documentType = target.Item.Metadata;
 
             var validationResult = new ValidationResult();
-            //выполняем валидацию на error
-            if (defaultBusinessProcess.Transitions[0].ValidationRuleError != null)
+
+            // TODO: Что это за проверка?
+            if (!string.Equals(configuration, "restfulapi", StringComparison.OrdinalIgnoreCase) && !string.Equals(configuration, "systemconfig", StringComparison.OrdinalIgnoreCase))
             {
-                var validationOperator =
-                    target.Context.GetComponent<IMetadataComponent>()
-                          .GetMetadata(target.Item.Configuration, target.Item.Metadata,
-                              MetadataType.ValidationError,
-                              (string)defaultBusinessProcess.Transitions[0].ValidationRuleError)
-                          .ValidationOperator;
-                IValidationOperator op = ValidationExtensions.CreateValidatorFromConfigValidator(validationOperator);
-                op.Validate(target.Item.Document, validationResult);
-            }
+                var defaultBusinessProcess = _metadataComponent.GetMetadata(configuration, documentType, MetadataType.Process, "Default");
 
+                // Скрипт, который выполняется для проверки документов на корректность
+                string onValidateAction = defaultBusinessProcess?.Transitions?[0]?.ValidationPointError?.ScenarioId;
 
-            if (defaultBusinessProcess.Transitions[0].ValidationPointError != null)
-            {
-                //получаем конструктор метаданных конфигураций
-                var configBuilder =
-                    target.Context.GetComponent<IConfigurationMediatorComponent>().ConfigurationBuilder;
-
-                IConfigurationObject configurationObject = configBuilder.GetConfigurationObject(target.Item
-                                                                                                      .Configuration);
-
-                if (configurationObject == null)
+                if (onValidateAction != null)
                 {
-                    Logger.Log.Error(Resources.ConfigurationReferencedInObjectNotFound, new Dictionary<string, object>
-                                                                                        {
-                                                                                            { "configuration", target.Item.Configuration },
-                                                                                            { "object", "DefaultBusinessProcess" },
-                                                                                        });
-                }
-                else
-                {
-                    var validationScenarioId = defaultBusinessProcess.Transitions[0].ValidationPointError.ScenarioId;
+                    var documents = target.Item.Documents;
 
-                    if (validationScenarioId != null)
+                    if (documents != null)
                     {
-                        var scriptProcessor = configurationObject.MetadataConfiguration.ScriptConfiguration.GetScriptProcessor();
-
-                        var context = new ApplyContext();
-                        context.CopyPropertiesFrom(target);
-                        context.Item = target.Item.Document ?? target.Item;
-                        context.Context = target.Context.GetComponent<ICustomServiceGlobalContext>();
-
-                        scriptProcessor.InvokeScript(validationScenarioId, context);
-
-                        if (context.ValidationMessage != null)
+                        foreach (var document in documents)
                         {
-                            if (context.ValidationMessage is IEnumerable &&
-                                context.ValidationMessage.GetType() != typeof(string))
+                            var actionContext = new ApplyContext();
+                            actionContext.CopyPropertiesFrom(target);
+                            actionContext.Item = document;
+                            actionContext.Item.Configuration = configuration;
+                            actionContext.Item.Metadata = documentType;
+                            actionContext.Context = _customServiceGlobalContext;
+
+                            _scriptRunnerComponent.InvokeScript(onValidateAction, actionContext);
+
+                            if (actionContext.ValidationMessage != null)
                             {
-                                validationResult.Items.AddRange(context.ValidationMessage);
+                                if (actionContext.ValidationMessage is IEnumerable && actionContext.ValidationMessage.GetType() != typeof(string))
+                                {
+                                    validationResult.Items.AddRange(actionContext.ValidationMessage);
+                                }
+                                else
+                                {
+                                    validationResult.Items.Add(actionContext.ValidationMessage);
+                                }
                             }
-                            else
-                            {
-                                validationResult.Items.Add(context.ValidationMessage);
-                            }
+
+                            validationResult.IsValid &= actionContext.IsValid;
                         }
-                        validationResult.IsValid &= context.IsValid;
                     }
                 }
             }
@@ -112,7 +78,6 @@ namespace InfinniPlatform.RestfulApi.DefaultProcessUnits
             target.Item.IgnoreWarnings = null;
             target.Item.ValidationWarnings = null;
             target.Item.ValidationErrors = null;
-
 
             target.ValidationMessage = validationResult.Items;
             target.IsValid = validationResult.IsValid;
--- InfinniPlatform.RestfulApi/DefaultProcessUnits/ValidationUnitSetDocumentWarning.cs
+++ InfinniPlatform.RestfulApi/DefaultProcessUnits/ValidationUnitSetDocumentWarning.cs
@@ -1,129 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 
-using InfinniPlatform.Api.ContextTypes.ContextImpl;
-using InfinniPlatform.Api.Metadata;
-using InfinniPlatform.Api.Properties;
-using InfinniPlatform.Api.Validation;
-using InfinniPlatform.Logging;
-using InfinniPlatform.Sdk.ContextComponents;
 using InfinniPlatform.Sdk.Contracts;
-using InfinniPlatform.Sdk.Environment;
-using InfinniPlatform.Sdk.Environment.Log;
-using InfinniPlatform.Sdk.Environment.Metadata;
-using InfinniPlatform.Sdk.Environment.Validations;
-using InfinniPlatform.Sdk.Global;
 
 namespace InfinniPlatform.RestfulApi.DefaultProcessUnits
 {
-    /// <summary>
-    ///     Валидатор для применения правил проверки документа, описанных в его метаданных
-    /// </summary>
+    [Obsolete]
     public sealed class ValidationUnitSetDocumentWarning
     {
         public void Action(IApplyContext target)
         {
-            dynamic validationMessage = target.ValidationMessage;
-
-            dynamic defaultBusinessProcess = null;
-
-            //TODO Игнорировать системные конфигурации при валидации. Пока непонятно, как переделать
-            if (target.Item.Configuration.ToLowerInvariant() != "systemconfig" &&
-                target.Item.Configuration.ToLowerInvariant() != "update" &&
-                target.Item.Configuration.ToLowerInvariant() != "restfulapi")
-            {
-                //ищем метаданные бизнес-процесса по умолчанию документа 
-                defaultBusinessProcess =
-                    target.Context.GetComponent<IMetadataComponent>()
-                          .GetMetadata(target.Item.Configuration, target.Item.Metadata,
-                              MetadataType.Process, "Default");
-            }
-
-            if (defaultBusinessProcess == null || defaultBusinessProcess.Transitions.Count == 0)
-            {
-                target.ValidationMessage = validationMessage;
-                return;
-            }
-
-            var validationResult = new ValidationResult();
-            //выполняем валидацию документа на warning
-            if ((target.Item.IgnoreWarnings == null || target.Item.IgnoreWarnings == false) &&
-                defaultBusinessProcess.Transitions[0].ValidationRuleWarning != null)
-            {
-                var validationOperator =
-                    target.Context.GetComponent<IMetadataComponent>()
-                          .GetMetadata((string)target.Item.Configuration, (string)target.Item.Metadata,
-                              MetadataType.ValidationWarning,
-                              (string)defaultBusinessProcess.Transitions[0].ValidationRuleWarning)
-                          .ValidationOperator;
-
-                //.GetValidationWarning(target.Item.Configuration, target.Item.Metadata,
-                //							  defaultBusinessProcess.Transitions[0].ValidationRuleWarning).ValidationOperator;
-
-                IValidationOperator op = ValidationExtensions.CreateValidatorFromConfigValidator(validationOperator);
-                op.Validate(target.Item.Document, validationResult);
-            }
-
-            //TODO исправить дублирование с ValidationUnitSetDocumentError
-            if (defaultBusinessProcess.Transitions[0].ValidationPointWarning != null)
-            {
-                //получаем конструктор метаданных конфигураций
-                var configBuilder =
-                    target.Context.GetComponent<IConfigurationMediatorComponent>().ConfigurationBuilder;
-
-                IConfigurationObject configurationObject = configBuilder.GetConfigurationObject(target.Item
-                                                                                                      .Configuration);
-
-                if (configurationObject == null)
-                {
-                    Logger.Log.Error(Resources.ConfigurationReferencedInObjectNotFound, new Dictionary<string, object>
-                                                                                        {
-                                                                                            { "configuration", target.Item.Configuration },
-                                                                                            { "object", "DefaultBusinessProcess" },
-                                                                                        });
-                }
-                else
-                {
-                    if (defaultBusinessProcess.Transitions[0].ValidationPointWarning.ScenarioId != null)
-                    {
-                        IValidationOperator validator = configurationObject.MetadataConfiguration.ScriptConfiguration
-                                                                           .GetValidator(
-                                                                               defaultBusinessProcess.Transitions[0]
-                                                                                   .ValidationPointWarning.ScenarioId);
-
-
-                        var context = new ApplyContext();
-
-                        context.CopyPropertiesFrom(target);
-                        context.Item = target.Item.Document ?? target.Item;
-                        context.Context = target.Context.GetComponent<ICustomServiceGlobalContext>();
-                        validator.Validate(context, new ValidationResult());
-
-                        if (context.ValidationMessage != null)
-                        {
-                            if (context.ValidationMessage is IEnumerable &&
-                                context.ValidationMessage.GetType() != typeof(string))
-                            {
-                                validationResult.Items.AddRange(context.ValidationMessage);
-                            }
-                            else
-                            {
-                                validationResult.Items.Add(context.ValidationMessage);
-                            }
-                        }
-                        validationResult.IsValid &= context.IsValid;
-                    }
-                }
-            }
-
-            target.Item.IgnoreWarnings = null;
-            target.Item.ValidationWarnings = null;
-            target.Item.ValidationErrors = null;
-
-
-            //заполняем результат сложной валидации
-            target.ValidationMessage = validationResult.Items;
-            target.IsValid = validationResult.IsValid;
         }
     }
 }
