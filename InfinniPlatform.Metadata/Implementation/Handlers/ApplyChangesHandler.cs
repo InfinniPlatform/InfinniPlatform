bc3a1c8c3ee503197581a0e7975ea0df727638bc
Sergey Pevnev <s.pevnev@infinnitysolutions.com>
2015-12-25 15:50:14 +0500

    PC-2559: Решить проблему с сохранением документов в кластерном режиме #resolved

--- InfinniPlatform.Metadata/Implementation/Handlers/ApplyChangesHandler.cs
+++ InfinniPlatform.Metadata/Implementation/Handlers/ApplyChangesHandler.cs
@@ -45,6 +45,8 @@ namespace InfinniPlatform.Metadata.Implementation.Handlers
                 throw new ArgumentException("document type undefined");
             }
 
+            changesObject.Documents = changesObject.Documents ?? new object[] { changesObject.Document } ;
+
             // Метаданные конфигурации
             var metadataConfiguration = _metadataConfigurationProvider.GetMetadataConfiguration(ConfigRequestProvider.GetConfiguration());
 
