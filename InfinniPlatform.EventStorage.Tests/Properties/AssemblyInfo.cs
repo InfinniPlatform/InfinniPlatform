8272601042ece69c0241ac99e607da54af3ccb74
AlexMAS <as.mezhov@gmail.com>
2016-01-11 08:47:39 +0500

    Delete InfinniPlatform.EventStorage

--- InfinniPlatform.EventStorage.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("InfinniPlatform.EventStorage.Tests")]
-[assembly: AssemblyDescription("InfinniPlatform.EventStorage.Tests")]
-[assembly: Guid("82baa72c-42f8-4aab-8aaa-98b93513e5a0")]
