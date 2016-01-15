e0b2a2d1bdca99e162938a0630bdf52980aaef89
AlexMAS <as.mezhov@gmail.com>
2016-01-16 01:39:32 +0500

    Delete WebApi #crazy

--- InfinniPlatform.WebApi.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("InfinniPlatform.WebApi.Tests")]
-[assembly: AssemblyDescription("InfinniPlatform.WebApi.Tests")]
-[assembly: Guid("741b0a83-c0c1-4743-815e-f4beb8f00931")]
