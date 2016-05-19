e48b01fb9e3c05514ae5fec17dff6194605f6e6d
Sergey Pevnev <s.pevnev@infinnitysolutions.com>
2016-05-19 14:41:21 +0500

    UserInterface #deleted

--- InfinniPlatform.UserInterface/ViewBuilders/DataElements/CodeEditor/ValidateRoutedEventHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace InfinniPlatform.UserInterface.ViewBuilders.DataElements.CodeEditor
-{
-    /// <summary>
-    ///     Делегат события проверки текста.
-    /// </summary>
-    public delegate void ValidateRoutedEventHandler(object sender, ValidateRoutedEventArgs e);
-}
--- InfinniPlatform.UserInterface/ViewBuilders/DataElements/RenderItemRoutedEventHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace InfinniPlatform.UserInterface.ViewBuilders.DataElements
-{
-    /// <summary>
-    ///     Делегат события отрисовки элемента.
-    /// </summary>
-    public delegate void RenderItemRoutedEventHandler(object sender, RenderItemRoutedEventArgs e);
-}
