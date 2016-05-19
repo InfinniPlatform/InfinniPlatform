90957f9bfa5e0880dccb1a33a2758b310206291b
Sergey Pevnev <s.pevnev@infinnitysolutions.com>
2016-05-19 14:44:49 +0500

    Cassandra #deleted

--- InfinniPlatform.Cassandra/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("InfinniPlatform.Cassandra")]
-[assembly: AssemblyDescription("InfinniPlatform.Cassandra")]
-[assembly: Guid("6fc517b7-2353-4402-a24b-b2b5a53c6a1a")]
-[assembly: InternalsVisibleTo("InfinniPlatform.Cassandra.Tests")]
