e35ce6140f72715306f6ad1e705c99675e265e50
Sergey Pevnev <s.pevnev@infinnitysolutions.com>
2016-07-04 09:30:45 +0500

    Added fake cache http service

---
+++ InfinniPlatform.Authentication.Tests/AnotherFakeHttpService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using InfinniPlatform.Sdk.Cache;
+using InfinniPlatform.Sdk.Services;
+
+namespace InfinniPlatform.Authentication.Tests
+{
+    public class CacheHttpServiceOne : IHttpService
+    {
+        public CacheHttpServiceOne(IMemoryCache memoryCache,
+                                   ISharedCache sharedCache,
+                                   ITwoLayerCache twoLayerCache)
+        {
+            _memoryCache = memoryCache;
+            _sharedCache = sharedCache;
+            _twoLayerCache = twoLayerCache;
+        }
+
+        private readonly IMemoryCache _memoryCache;
+        private readonly ISharedCache _sharedCache;
+        private readonly ITwoLayerCache _twoLayerCache;
+
+        public void Load(IHttpServiceBuilder builder)
+        {
+            builder.ServicePath = "/cacheo";
+            builder.Get["/set"] = Set;
+        }
+
+        private Task<object> Set(IHttpRequest httpRequest)
+        {
+            _memoryCache.Clear();
+            _memoryCache.Set("memory", "memory");
+            _sharedCache.Clear();
+            _sharedCache.Set("shared", "shared");
+            _twoLayerCache.Clear();
+            _twoLayerCache.Set("twoLayer", "twoLayer");
+
+            return Task.FromResult<object>("Ok.");
+        }
+    }
+
+
+    public class CacheHttpServiceTwo : IHttpService
+    {
+        public CacheHttpServiceTwo(IMemoryCache memoryCache,
+                                   ISharedCache sharedCache,
+                                   ITwoLayerCache twoLayerCache)
+        {
+            _memoryCache = memoryCache;
+            _sharedCache = sharedCache;
+            _twoLayerCache = twoLayerCache;
+        }
+
+        private readonly IMemoryCache _memoryCache;
+        private readonly ISharedCache _sharedCache;
+        private readonly ITwoLayerCache _twoLayerCache;
+
+        public void Load(IHttpServiceBuilder builder)
+        {
+            builder.ServicePath = "/cachet";
+            builder.Get["/get"] = Get;
+        }
+
+        private Task<object> Get(IHttpRequest httpRequest)
+        {
+            var s = _memoryCache.Get("memory");
+            var s1 = _sharedCache.Get("shared");
+            var s2 = _twoLayerCache.Get("twoLayer");
+
+            return Task.FromResult<object>($"memory: {s}{Environment.NewLine}shared: {s1}{Environment.NewLine}twoLayer: {s2}");
+        }
+    }
+}
