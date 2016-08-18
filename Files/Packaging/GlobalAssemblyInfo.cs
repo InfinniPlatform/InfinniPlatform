0d05cfe6589b2e71d3bacc616c00e071dce21b3b
AlexMAS <as.mezhov@gmail.com>
2016-08-18 13:24:51 +0500

    v 1.10

--- Files/Packaging/GlobalAssemblyInfo.cs
+++ Files/Packaging/GlobalAssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.InteropServices;
 // Find what: ((AssemblyVersion|AssemblyFileVersion)\s*\(\s*@?\")(?<major>[0-9]+)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)\.(?<build>[0-9]+)(\"\s*\))
 // Replace with: $1$3.$4.$5.\%build.number%$7
 
-[assembly: AssemblyVersion("1.9.0.0")]
-[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyVersion("1.10.0.0")]
+[assembly: AssemblyFileVersion("1.10.0.0")]
 
 // TeamCity File Content Replacer: Add VCS hash
 // Look in: */Packaging/GlobalAssemblyInfo.cs
