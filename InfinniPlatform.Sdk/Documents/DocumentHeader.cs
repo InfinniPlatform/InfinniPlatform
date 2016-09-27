e38b4d1968ed04bf9dbb9ebc65b19592097d1656
AlexMAS <as.mezhov@gmail.com>
2016-09-27 09:56:57 +0500

    MC-4407: Mark members of DocumentHeader as SerializerVisible

--- InfinniPlatform.Sdk/Documents/DocumentHeader.cs
+++ InfinniPlatform.Sdk/Documents/DocumentHeader.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using InfinniPlatform.Sdk.Serialization;
+
 namespace InfinniPlatform.Sdk.Documents
 {
     /// <summary>
@@ -13,54 +15,64 @@ namespace InfinniPlatform.Sdk.Documents
         /// <summary>
         /// Идентификатор арендатора сервиса.
         /// </summary>
+        [SerializerVisible]
         public string _tenant { get; internal set; }
 
 
         /// <summary>
         /// Дата создания документа.
         /// </summary>
+        [SerializerVisible]
         public DateTime? _created { get; internal set; }
 
         /// <summary>
         /// Имя пользователя, который создал документ.
         /// </summary>
+        [SerializerVisible]
         public string _createUser { get; internal set; }
 
         /// <summary>
         /// Идентификатор пользователя, который создал документ.
         /// </summary>
+        [SerializerVisible]
         public string _createUserId { get; internal set; }
 
 
         /// <summary>
         /// Дата последнего изменения документа.
         /// </summary>
+        [SerializerVisible]
         public DateTime? _updated { get; internal set; }
 
         /// <summary>
         /// Имя пользователя, который последний изменил документ.
         /// </summary>
+        [SerializerVisible]
         public string _updateUser { get; internal set; }
 
         /// <summary>
         /// Идентификатор пользователя, который последний изменил документ.
         /// </summary>
+        [SerializerVisible]
         public string _updateUserId { get; internal set; }
 
 
         /// <summary>
         /// Дата удаления документа.
         /// </summary>
+        [SerializerVisible]
         public DateTime? _deleted { get; internal set; }
 
         /// <summary>
         /// Имя пользователя, который удалил документ.
         /// </summary>
+        [SerializerVisible]
         public string _deleteUser { get; internal set; }
 
         /// <summary>
         /// Идентификатор пользователя, который удалил документ.
         /// </summary>
+        [SerializerVisible]
         public string _deleteUserId { get; internal set; }
 
 
