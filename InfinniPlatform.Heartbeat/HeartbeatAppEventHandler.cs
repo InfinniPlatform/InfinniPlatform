7d632f079d822e8be958681d9e2ab1b7285b3078
Sergey Pevnev <larockarocka@hotmail.com>
2016-10-18 10:53:09 +0500

    [Heartbeat] Add settings

---
+++ InfinniPlatform.Heartbeat/HeartbeatAppEventHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+
+using InfinniPlatform.Heartbeat.Settings;
+using InfinniPlatform.Sdk.Hosting;
+
+namespace InfinniPlatform.Heartbeat
+{
+    public class HeartbeatAppEventHandler : AppEventHandler
+    {
+        public HeartbeatAppEventHandler(HeartbeatSettings settings)
+        {
+            _settings = settings;
+        }
+
+        private readonly HeartbeatSettings _settings;
+
+        public override void OnAfterStart()
+        {
+            //send to server
+            var period = _settings.Period;
+
+            Task.Run(async () =>
+                     {
+                         while (true)
+                         {
+                             await Task.Delay(period);
+                         }
+                         // ReSharper disable once FunctionNeverReturns
+                     });
+        }
+
+        public override void OnAfterStop()
+        {
+            base.OnAfterStop();
+            //send to server
+        }
+    }
+}
---
+++ InfinniPlatform.Heartbeat/Settings/HeartbeatSettings.cs
@@ -0,0 +1,27 @@
+﻿namespace InfinniPlatform.Heartbeat.Settings
+{
+    public class HeartbeatSettings
+    {
+        public const string SectionName = "heartbeatSettings";
+
+        public HeartbeatSettings()
+        {
+            Period = 5;
+        }
+
+        /// <summary>
+        /// Периодичность оповещения сервера (в секундах).
+        /// </summary>
+        public int Period { get; set; }
+
+        /// <summary>
+        /// Адрес сервера InfinniPlatform.Server.
+        /// </summary>
+        public string ServerAddress { get; set; }
+
+        /// <summary>
+        /// Уникальный token для аутентификации при обмене данными между агентом и сервером.
+        /// </summary>
+        public string Token { get; set; }
+    }
+}
