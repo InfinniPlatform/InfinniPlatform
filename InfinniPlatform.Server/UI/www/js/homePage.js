a025bb105e0e9793a3f5f22d612e8a561773ed54
Sergey Pevnev <larockarocka@hotmail.com>
2016-10-24 17:56:17 +0500

    Add parameters to config view

--- InfinniPlatform.Server/UI/www/js/homePage.js
+++ InfinniPlatform.Server/UI/www/js/homePage.js
@@ -2,17 +2,24 @@ function ShowAppsPanel(context) {
     context.controls.AppsPanel.setVisible(true);
 }
 
-function PostConfigFile(context) {
-    var item = context.dataSources.ConfigDataSource.getSelectedItem();
-    var data = { "Config": JSON.parse(item) };
+function PostAppExtensionConfig(context) {
+    var item = context.dataSources.AppExtensionSource.getSelectedItem();
+    var data = JSON.stringify({ "Config": JSON.parse(item) });
+
+    var args = [window.InfinniUI.config.serverUrl,
+    context.parameters.AgentAddress.getValue(),
+    context.parameters.AgentPort.getValue(),
+    context.parameters.AppFullName.getValue()];
+
+    var url = InfinniUI.StringUtils.format("{0}/server/config?Address={1}&Port={2}&AppFullName={3}&FileName=AppExtension.json", args);
 
     $.ajax({
         type: 'post',
-        url: "http://localhost:9901/server/config?Address=localhost&Port=9901&AppFullName=UpravdomGkh.1.1.0.2133-default&FileName=AppExtension.json",
+        url: url,
         xhrFields: {
             withCredentials: true
         },
-        data: JSON.stringify(data),
+        data: data,
         contentType: "application/json;charset=UTF-8"
     });
 }
