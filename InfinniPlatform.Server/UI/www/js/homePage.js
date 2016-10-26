17fc9b48844da5085f018c81b887f42fdaa4ca9d
Sergey Pevnev <larockarocka@hotmail.com>
2016-10-26 17:39:38 +0500

    [Server] Fancy real-time grid updates

--- InfinniPlatform.Server/UI/www/js/homePage.js
+++ InfinniPlatform.Server/UI/www/js/homePage.js
@@ -4,12 +4,15 @@ function ShowAppsPanel(context) {
 
 function PostAppExtensionConfig(context) {
     var config = context.dataSources.AppExtensionSource.getSelectedItem();
-    var data = JSON.stringify({ "Config": JSON.parse(config) });
+    var data = JSON.stringify({
+        "Config": JSON.parse(config)
+    });
 
     var args = [window.InfinniUI.config.serverUrl,
-    context.parameters.AgentAddress.getValue(),
-    context.parameters.AgentPort.getValue(),
-    context.parameters.AppFullName.getValue()];
+        context.parameters.AgentAddress.getValue(),
+        context.parameters.AgentPort.getValue(),
+        context.parameters.AppFullName.getValue()
+    ];
 
     var url = InfinniUI.StringUtils.format("{0}/server/config?Address={1}&Port={2}&AppFullName={3}&FileName=AppExtension.json", args);
 
@@ -22,4 +25,13 @@ function PostAppExtensionConfig(context) {
         data: data,
         contentType: "application/json;charset=UTF-8"
     });
+}
+
+function ShowInstallationLog(context, args) {
+    context.controls.InstallLogTextBox.setVisible(true);
+    context.controls.InstallLogTextBox.setValue(args.data.Result.Output);
+}
+
+function RefreshAppsDataSource(context) {
+    context.dataSources.AppsDataSource.updateItems();
 }
