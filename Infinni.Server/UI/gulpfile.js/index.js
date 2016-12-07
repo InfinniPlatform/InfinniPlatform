91bf41c2ed8376fab5f2d1968e00d70137f05ffa
laroc <larockarocka@hotmail.com>
2016-12-07 12:18:26 +0500

    Remove npm warnings

--- Infinni.Server/UI/gulpfile.js/index.js
+++ Infinni.Server/UI/gulpfile.js/index.js
@@ -1,17 +1,18 @@
 ﻿'use strict';
 var help = '';
 var gulp = require('gulp'),
-			watch = require('gulp-watch'),
-			sourceForTasks = require('./gulptasks/sourceForTasks'),
-			lazyRequireTask = function(taskName, path, options) {
-				options = options || {};
-				options.taskName = taskName;
-				gulp.task(taskName, function(callback) {
-					var task = require(path).call(this, options);
-					return task(callback);
-				});
-			};
-for(var key in sourceForTasks) {
+	watch = require('gulp-watch'),
+	sourceForTasks = require('./gulptasks/sourceForTasks'),
+	lazyRequireTask = function (taskName, path, options) {
+		options = options || {};
+		options.taskName = taskName;
+		gulp.task(taskName, function (callback) {
+			var task = require(path).call(this, options);
+			return task(callback);
+		});
+	};
+	
+for (var key in sourceForTasks) {
 	help += '- gulp ' + key + '\n';
 	lazyRequireTask(key, sourceForTasks[key].taskPath, sourceForTasks[key]);
 }
@@ -20,20 +21,20 @@ gulp.task('build', gulp.series(
 	gulp.parallel(gulp.series('copyPlatform', 'overrideLess', 'copyApp', 'copyAssemblies'), 'concatJs')
 ));
 
-gulp.task('fullWatch', function() {
+gulp.task('fullWatch', function () {
 	watch(sourceForTasks.copyPlatform.src, gulp.series('copyPlatform', 'overrideLess'));
 	watch(sourceForTasks.copyApp.src, gulp.series('copyApp'));
 	watch(sourceForTasks.copyAssemblies.src, gulp.series('copyAssemblies'));
 	watch(sourceForTasks.overrideLess.srcForWatch, gulp.series('overrideLess'));
-	watch(sourceForTasks.concatJs.src, gulp.series('concatJs'));	
+	watch(sourceForTasks.concatJs.src, gulp.series('concatJs'));
 });
 
-gulp.task('default', function(cb) {
+gulp.task('default', function (cb) {
 	console.log('####Task is not defined!\n' +
-							'####Use any of defined tasks:\n' +
-							help +
-							'- gulp build\n' +
-							'- gulp example'
-							);
+		'####Use any of defined tasks:\n' +
+		help +
+		'- gulp build\n' +
+		'- gulp example'
+	);
 	cb();
 });
