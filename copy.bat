f7c7b3e230923f829215ada27e72cfad7bab1abd
Sergey Pevnev <larockarocka@hotmail.com>
2017-02-07 13:27:59 +0500

    Revert PostBuildScript, update NETCore.App to 1.1

--- copy.bat
+++ copy.bat
@@ -1,14 +1,14 @@
-REM echo off
+echo off
 
-REM SET "ASSEMBLIES=%~dp0Assemblies\"
-REM SET "ASSEMBLIES_RU=%~dp0Assemblies\ru-RU\"
-REM SET "OUTPUT=%~dp0%1\bin\Debug\%2\"
-REM SET "OUTPUT_RU=%~dp0%1\bin\Debug\%2\ru-RU\"
+SET "ASSEMBLIES=%~dp0Assemblies\"
+SET "ASSEMBLIES_RU=%~dp0Assemblies\ru-RU\"
+SET "OUTPUT=%~dp0%1\bin\Debug\%2\"
+SET "OUTPUT_RU=%~dp0%1\bin\Debug\%2\ru-RU\"
 
-REM pushd %OUTPUT%
-REM for %%i in (*) do xcopy /Y /F %%i %ASSEMBLIES%
-REM popd
+pushd %OUTPUT%
+for %%i in (*) do xcopy /Y /F %%i %ASSEMBLIES%
+popd
 
-REM pushd %OUTPUT_RU%
-REM for %%i in (*) do xcopy /Y /F %%i %ASSEMBLIES_RU%
-REM popd
+pushd %OUTPUT_RU%
+for %%i in (*) do xcopy /Y /F %%i %ASSEMBLIES_RU%
+popd
