e83103dae3b0a3fdd235e9b3b4d077a8db6a656d
Sergey Pevnev <larockarocka@hotmail.com>
2017-04-14 17:23:35 +0500

    [MessageQueue] Rename .csproj

--- InfinniPlatform.MessageQueue.RabbitMQ/Properties/AssemblyInfo.cs
+++ InfinniPlatform.MessageQueue.RabbitMQ/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("InfinniPlatform.MessageQueue")]
-[assembly: AssemblyDescription("InfinniPlatform.MessageQueue")]
+[assembly: AssemblyTitle("InfinniPlatform.MessageQueue.RabbitMQ")]
+[assembly: AssemblyDescription("InfinniPlatform.MessageQueue.RabbitMQ")]
 [assembly: Guid("fd554eef-b3c4-4ac5-8f32-7b23a0493e73")]
 [assembly: InternalsVisibleTo("InfinniPlatform.MessageQueue.Tests")]
