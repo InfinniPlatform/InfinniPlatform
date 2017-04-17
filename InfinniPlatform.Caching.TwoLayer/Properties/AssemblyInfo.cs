084487e0ed8801d2da08a4f23a2b10c652e2875f
laroc <larockarocka@hotmail.com>
2017-04-17 16:36:23 +0500

    [Caching] Add implementations

---
+++ InfinniPlatform.Caching.TwoLayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InfinniPlatform.Caching.TwoLayer")]
+[assembly: AssemblyDescription("InfinniPlatform.Caching.TwoLayer")]
+[assembly: Guid("4b2cdfba-e609-41a2-a9cf-695d73fd8555")]
