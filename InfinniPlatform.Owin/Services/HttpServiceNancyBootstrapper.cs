8259f10a951d237f79e6cdc236e3e10480d62a47
Sergey Pevnev <larockarocka@hotmail.com>
2017-08-01 17:40:18 +0500

    Tune OWIN and Nancy stream behaviour

--- InfinniPlatform.Owin/Services/HttpServiceNancyBootstrapper.cs
+++ InfinniPlatform.Owin/Services/HttpServiceNancyBootstrapper.cs
@@ -69,8 +69,6 @@ namespace InfinniPlatform.Owin.Services
 
         protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
         {
-            StaticConfiguration.DisableRequestStreamSwitching = true;
-
             // Проверка заголовка Last-Modified при обработке запросов к файлам.
             pipelines.AfterRequest += CheckForIfModifiedSince;
             pipelines.AfterRequest += CheckForIfNonMatch;
