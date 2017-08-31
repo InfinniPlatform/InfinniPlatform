c184ca683b13da2a589bd93be2fa6fd3fb613b0a
Sergey Pevnev <larockarocka@hotmail.com>
2017-08-31 17:04:26 +0500

    Remove aspect from test http service

--- InfinniPlatform.ServiceHost/HttpService.cs
+++ InfinniPlatform.ServiceHost/HttpService.cs
@@ -2,19 +2,15 @@
 using InfinniPlatform.DocumentStorage;
 using InfinniPlatform.Dynamic;
 using InfinniPlatform.Http;
-using Microsoft.Extensions.Logging;
 
 namespace InfinniPlatform.ServiceHost
 {
     public class HttpService : IHttpService
     {
         private readonly IDocumentStorageProvider _documentStorageProvider;
-        private readonly ILogger<HttpService> _logger;
 
-        public HttpService(IDocumentStorageProviderFactory storageFactory,
-                           ILogger<HttpService> logger)
+        public HttpService(IDocumentStorageProviderFactory storageFactory)
         {
-            _logger = logger;
             _documentStorageProvider = storageFactory.GetStorageProvider("Documents");
         }
 
@@ -23,17 +19,13 @@ namespace InfinniPlatform.ServiceHost
             builder.Post["/save"] = Save;
         }
 
-        private async Task<object> Save(IHttpRequest httpRequest)
+        public async Task<object> Save(IHttpRequest httpRequest)
         {
-            _logger.LogInformation($"Starting saving doc. IMMA {httpRequest.User?.Name}.");
-
             var doc = new DynamicDocument {{"Field1", "Value1"}};
 
             await _documentStorageProvider.InsertOneAsync(doc);
 
-            _logger.LogInformation($"Finished saving doc. IMMA {httpRequest.User?.Name}.");
-
-            return "Ok.";
+            return doc;
         }
     }
 }
