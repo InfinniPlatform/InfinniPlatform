6cd587f742d662409a9807319e18c873f631e79e
Sergey Pevnev <larockarocka@hotmail.com>
2017-09-25 12:12:48 +0500

    Fix nupkg build

--- Files/Packaging/BuildNupkg.ps1
+++ Files/Packaging/BuildNupkg.ps1
@@ -4,23 +4,23 @@
 #>
 param
 (
-    [Parameter(HelpMessage = "Path to the solution directory.")]
-    [String] $solutionDir = '.',
+  [Parameter(HelpMessage = "Path to the solution directory.")]
+  [String] $solutionDir = '.',
 
-    [Parameter(HelpMessage = "Path to the solution output directory.")]
-    [String] $outputDir = 'Assemblies',
+  [Parameter(HelpMessage = "Path to the solution output directory.")]
+  [String] $outputDir = 'Assemblies',
 
-    [Parameter(HelpMessage = "Path to GlobalAssemblyInfo.cs.")]
-    [String] $assemblyInfo = 'Files\Packaging\GlobalAssemblyInfo.cs',
+  [Parameter(HelpMessage = "Path to GlobalAssemblyInfo.cs.")]
+  [String] $assemblyInfo = 'Files\Packaging\GlobalAssemblyInfo.cs',
 
-    [Parameter(HelpMessage = "VCS branch name.")]
-    [String] $branchName = '',
+  [Parameter(HelpMessage = "VCS branch name.")]
+  [String] $branchName = 'release-1.11',
 
-    [Parameter(HelpMessage = "VCS commit hash.")]
-    [String] $commitHash = '',
+  [Parameter(HelpMessage = "VCS commit hash.")]
+  [String] $commitHash = '',
 
-    [Parameter(HelpMessage = "Build mode.")]
-    [String] $buildMode = 'Release'
+  [Parameter(HelpMessage = "Build mode.")]
+  [String] $buildMode = 'Release'
 )
 
 # Script dependencies
@@ -35,33 +35,37 @@ Remove-Item -Path $outputDir -Recurse -ErrorAction SilentlyContinue
 $nugetDir = Join-Path $env:ProgramData 'NuGet'
 $nugetPath = Join-Path $nugetDir 'nuget.exe'
 
-if (-not (Test-Path $nugetPath))
-{
-    if (-not (Test-Path $nugetDir))
-    {
-        New-Item $nugetDir -ItemType Directory -ErrorAction SilentlyContinue
-    }
+if (-not (Test-Path $nugetPath)) {
+  if (-not (Test-Path $nugetDir)) {
+    New-Item $nugetDir -ItemType Directory -ErrorAction SilentlyContinue
+  }
 
-    $nugetSourceUri = 'http://dist.nuget.org/win-x86-commandline/latest/nuget.exe'
-    Invoke-WebRequest -Uri $nugetSourceUri -OutFile $nugetPath
+  $nugetSourceUri = 'http://dist.nuget.org/win-x86-commandline/latest/nuget.exe'
+  Invoke-WebRequest -Uri $nugetSourceUri -OutFile $nugetPath
 }
 
 # Restore packages
 & "$nugetPath" restore 'InfinniPlatform.sln' -NonInteractive
 
 # Build the solution
-& "${Env:ProgramFiles(x86)}\MSBuild\14.0\bin\msbuild.exe" 'InfinniPlatform.sln' /t:Clean /p:Configuration=$buildMode /verbosity:quiet /consoleloggerparameters:ErrorsOnly
-& "${Env:ProgramFiles(x86)}\MSBuild\14.0\bin\msbuild.exe" 'InfinniPlatform.sln' /p:Configuration=$buildMode /verbosity:quiet /consoleloggerparameters:ErrorsOnly
+$msbuildPath = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
-# Create nuspec-files
-Create-Nuspec `
+if (-not (Test-Path $msbuildPath)) {
+  Write-Error "Build Tools 2017 must be installed."
+}
+else {
+  & $msbuildPath 'InfinniPlatform.sln' /t:Clean /p:Configuration=$buildMode /verbosity:quiet /consoleloggerparameters:ErrorsOnly /m
+  & $msbuildPath 'InfinniPlatform.sln' /p:Configuration=$buildMode /verbosity:quiet /consoleloggerparameters:ErrorsOnly /m
+
+  # Create nuspec-files
+  Create-Nuspec `
     -solutionDir $solutionDir `
     -outputDir $outputDir `
     -assemblyInfo $assemblyInfo `
     -branchName $branchName `
     -commitHash $commitHash
 
-Get-ChildItem $outputDir -Filter '*.nuspec' | Foreach-Object {
+  Get-ChildItem $outputDir -Filter '*.nuspec' | Foreach-Object {
     $nuspecFile = Join-Path $outputDir $_.Name
 
     # Create nupkg-file
@@ -69,4 +73,5 @@ Get-ChildItem $outputDir -Filter '*.nuspec' | Foreach-Object {
 
     # Remove nuspec-file
     Remove-Item -Path $nuspecFile
+  }
 }
