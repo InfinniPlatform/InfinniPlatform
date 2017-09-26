c4bc31707fef0b4d6c3cf2a4f918a87c663355f9
Sergey Pevnev <larockarocka@hotmail.com>
2017-09-26 16:00:14 +0500

    Bump version to 2.5.0.0

--- Files/Packaging/GlobalAssemblyInfo.cs
+++ Files/Packaging/GlobalAssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.InteropServices;
 // Find what: ((AssemblyVersion|AssemblyFileVersion)\s*\(\s*@?\")(?<major>[0-9]+)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)\.(?<build>[0-9]+)(\"\s*\))
 // Replace with: $1$3.$4.$5.\%build.number%$7
 
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
 
 // TeamCity File Content Replacer: Add VCS hash
 // Look in: */Packaging/GlobalAssemblyInfo.cs
