81ebeedc42a30bdfd1d2f9546193a32912a59728
Sergey Pevnev <larockarocka@hotmail.com>
2017-10-20 13:08:56 +0500

    Implement hosting as Windows service

---
+++ InfinniPlatform.WindowsServiceHost/WebHostExtensions.cs
@@ -0,0 +1,18 @@
+﻿using DasMulli.Win32.ServiceUtils;
+using InfinniPlatform.WindowsServiceHost;
+using Microsoft.AspNetCore.Hosting;
+
+// ReSharper disable once CheckNamespace
+namespace InfinniPlatform.AspNetCore
+{
+    public static class WebHostExtensions
+    {
+        public static void RunAsService(this IWebHost webHost)
+        {
+            var aspNetWin32Service = new AspNetWin32Service(nameof(AspNetWin32Service), webHost);
+            var serviceHost = new Win32ServiceHost(aspNetWin32Service);
+
+            serviceHost.Run();
+        }
+    }
+}
