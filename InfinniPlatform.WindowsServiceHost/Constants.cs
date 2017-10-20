55e4fd90aa9b091a8888e49d4274a50d2e9c31cd
Sergey Pevnev <larockarocka@hotmail.com>
2017-10-20 14:38:17 +0500

    Add option constant

---
+++ InfinniPlatform.WindowsServiceHost/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace InfinniPlatform
+{
+    public static class Constants
+    {
+        /// <summary>
+        /// Опция коммандной строки, передаваемая хосту, для запуска приложения в качестве службы Windows.
+        /// </summary>
+        public const string WindowsServiceOption = "--windows-service";
+    }
+}
