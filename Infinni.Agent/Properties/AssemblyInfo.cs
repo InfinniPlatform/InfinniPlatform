e64a1aba37d61f33c798af3fa1988792584309b3
Sergey Pevnev <larockarocka@hotmail.com>
2017-11-28 16:58:03 +0500

    Remove Infinni.Server and Infinni.Agent
    Deprecated and slow down build time on CI

--- Infinni.Agent/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Infinni.Agent")]
-[assembly: AssemblyDescription("Infinni.Agent")]
-[assembly: Guid("456c9a1e-f32e-4bb6-9ef6-fbbdf4c49db7")]
