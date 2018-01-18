fc5ea7245a6763dd10d36d795ef58ff7fe7207d6
Sergey Pevnev <larockarocka@hotmail.com>
2018-01-18 17:29:47 +0500

    Implement file uploads to blob storage

--- InfinniPlatform.BlobStorage.HttpService/BlobStorageHttpService.cs
+++ InfinniPlatform.BlobStorage.HttpService/BlobStorageHttpService.cs
@@ -1,30 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-
 using InfinniPlatform.BlobStorage.Properties;
 using InfinniPlatform.Http;
 using InfinniPlatform.Logging;
-
 using Microsoft.Extensions.Logging;
 
 namespace InfinniPlatform.BlobStorage
 {
     /// <summary>
-    /// Provides HTTP service to get data from <see cref="IBlobStorage"/>.
+    /// Provides HTTP service to get data from <see cref="IBlobStorage" />.
     /// </summary>
-    /// <example>
-    /// <code>
-    /// GET /blob/{id}
-    /// </code>
-    /// </example>
     [LoggerName(nameof(BlobStorageHttpService))]
     public class BlobStorageHttpService : IHttpService
     {
         public const string DefaultServicePath = "/blob";
 
 
-        public BlobStorageHttpService(IBlobStorage blobStorage, IPerformanceLogger<BlobStorageHttpService> perfLogger, ILogger<BlobStorageHttpService> logger)
+        private readonly IBlobStorage _blobStorage;
+        private readonly ILogger _logger;
+        private readonly IPerformanceLogger _perfLogger;
+
+
+        public BlobStorageHttpService(IBlobStorage blobStorage, 
+                                      IPerformanceLogger<BlobStorageHttpService> perfLogger, 
+                                      ILogger<BlobStorageHttpService> logger)
         {
             _blobStorage = blobStorage;
             _perfLogger = perfLogger;
@@ -32,20 +32,15 @@ namespace InfinniPlatform.BlobStorage
         }
 
 
-        private readonly IBlobStorage _blobStorage;
-        private readonly IPerformanceLogger _perfLogger;
-        private readonly ILogger _logger;
-
-
         public virtual void Load(IHttpServiceBuilder builder)
         {
             builder.ServicePath = DefaultServicePath;
 
             builder.Get["/{id}"] = GetFileContentAsync;
+            builder.Post["/upload"] = PostFileContentAsync;
         }
 
-
-        protected virtual Task<object> GetFileContentAsync(IHttpRequest request)
+        private Task<object> GetFileContentAsync(IHttpRequest request)
         {
             var startTime = DateTime.Now;
 
@@ -64,10 +59,10 @@ namespace InfinniPlatform.BlobStorage
                     if (blobData != null)
                     {
                         var fileResponse = new StreamHttpResponse(blobData.Data, blobData.Info.Type)
-                                           {
-                                               FileName = blobData.Info.Name,
-                                               LastWriteTimeUtc = blobData.Info.Time
-                                           };
+                        {
+                            FileName = blobData.Info.Name,
+                            LastWriteTimeUtc = blobData.Info.Time
+                        };
 
                         fileResponse.SetContentDispositionAttachment();
 
@@ -81,7 +76,7 @@ namespace InfinniPlatform.BlobStorage
             {
                 exception = e;
 
-                _logger.LogError(Resources.RequestProcessedWithException, e, () => new Dictionary<string, object> { { "method", method } });
+                _logger.LogError(Resources.RequestProcessedWithException, e, () => new Dictionary<string, object> {{"method", method}});
 
                 throw;
             }
@@ -89,6 +84,46 @@ namespace InfinniPlatform.BlobStorage
             {
                 _perfLogger.Log(method, startTime, exception);
             }
+        }
+
+        private async Task<object> PostFileContentAsync(IHttpRequest request)
+        {
+            var startTime = DateTime.Now;
+
+            var method = $"{request.Method}::{request.Path}";
+
+            var result = new ServiceResult<List<BlobInfo>>();
+
+            Exception exception = null;
+
+            try
+            {
+                var blobInfos = new List<BlobInfo>();
+
+                foreach (var file in request.Files)
+                {
+                    var blobInfo = await _blobStorage.CreateBlobAsync(file.Name, file.ContentType, file.Value);
+
+                    blobInfos.Add(blobInfo);
+                }
+
+                result.Result = blobInfos;
+                result.Success = true;
+            }
+            catch (Exception e)
+            {
+                exception = e;
+
+                _logger.LogError(Resources.RequestProcessedWithException, e, () => new Dictionary<string, object> {{"method", method}});
+
+                throw;
+            }
+            finally
+            {
+                _perfLogger.Log(method, startTime, exception);
+            }
+
+            return result;
         }
     }
 }
